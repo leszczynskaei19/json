16e66d306042a7f0c7df26be9418954ae189d7a7
leszczynskaei19 <102586122+leszczynskaei19@users.noreply.github.com>
2023-03-09 13:17:03 +0100

    Update cos.js

--- cos.js
+++ cos.js
@@ -1,25 +1,3 @@
-const data = [
-  { name: 'Marcin', age: 30, city: 'Gliwice', sex:"M"  },
-  { name: 'Tomek', age: 25, city: 'Sosnowiec', sex:"M" },
-  { name: 'Jacek', age: 40, city: 'Bytom', sex:"M" },
-  { name: 'Kasia', age: 21, city: 'Bytom', sex:"K" },
-];
+import pobranyJson from "http://imiki.pl/cf/" assert { type: "json" };
 
-const table = document.createElement('table');
-table.setAttribute('class', 'tabelka');
-const headerRow = table.insertRow();
-Object.keys(data[0]).forEach(key => {
-  const headerCell = headerRow.insertCell();
-  headerCell.textContent = key;
-});
-data.forEach(obj => {
-  const row = table.insertRow();
-  Object.values(obj).forEach(value => {
-    const cell = row.insertCell();
-    cell.textContent = value;
-  });
-});
-const json = JSON.stringify(data);
-console.log(json);
-
-document.body.appendChild(table);
+console.log(pobranyJson.data[0].state.temperature);
